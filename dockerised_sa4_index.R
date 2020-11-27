4aa69520d140b4001e024a7f944a49884423187e
Arv Hughes <arvind.hughes@hdr.mq.edu.au>
2020-11-27 12:33:24 +1100

    Added outlier removal & removed state dependencies

--- dockerised_sa4_index.R
+++ dockerised_sa4_index.R
@@ -45,60 +45,86 @@ print("Running House Index for NSW between phase 0-720")
 
 ##############
 ### Variables
-
-state<-'NSW'
 phase<-0:720
 property_type<-'house'
 sa4_indices<-data.frame(phase)
-
-
+outlier_threshold<-1
 #################
 
 
 
 ## Get SA4's for NSW
 query<-dbSendQuery(con,glue::glue(" SELECT distinct sa4_16code as sa4
-                                    FROM public.abs_geographies
-                                    where state = '{state}' "))
+                                    FROM public.abs_geographies"))
 sa4_NSW<-dbFetch(query)
 dbClearResult(query)
 #############################
 
 
+
+
+#############################
+### Truncate SA4_index table
+dbSendQuery(con, "TRUNCATE TABLE sa4_index_smoothed")
+#############################
+
+
+
 ###### Generate Index for each SA4 ( Creates individual dataframes for each)
-for (i in sa4_NSW$sa4){
+###### Generate Index for each SA4 ( Creates individual dataframes for each)
+for (i in sa4_NSW){
   print(paste0("Doing SA4 = ",i))
-  query<-dbSendQuery(con,glue::glue("select  sa4, phase, percentile_disc(0.5) within group (order by price) as median, count(price) as n
-        from (
-            select ag.sa4_16code as sa4, se.price, pl.phase
+  
+  query<-dbSendQuery(con,glue::glue("select ag.sa4_16code as sa4, se.price, pl.phase,se.address_id 
             from abs_geographies ag
             join sale_event se on se.address_id = ag.address_id
             JOIN phase_lookup pl
                 ON pl.YEAR = date_part('year', se.contract_date )
                 and pl.month = date_part('month', se.contract_date )
             JOIN address a ON a.id = ag.address_id
-            where a.property_type ='{property_type}'
+            where a.property_type ='{property_type}' and se.price > 200000 
             and ag.address_id in (
                 select ag.address_id from abs_geographies ag
                 join sale_event se on ag.address_id = se.address_id
                 where ag.sa4_16code = {i}
-                and se.price between 200000 and 6000000
-                group by ag.address_id having count(se.id) between 2 and 10
-            )
-            and se.price between 200000 and 6000000
-        ) index_data
-        group by sa4, phase
-        order by sa4 , phase
-        "))
+                group by ag.address_id having count(se.id) between 2 and 10)
+                                    "))
   
   sa4<-dbFetch(query)
   dbClearResult(query)
   
   
-  phase<-sa4$phase
-  assign(paste0('index_',i),data.frame(indexVal=priceIndex( "median","n", 1, sa4,method="Laspeyres" ),phase))
-  #sa4_indices<-merge(sa4_indices,data.frame(assign(paste0('index_',i),priceIndex( "median","n", 1, sa4,method="Laspeyres" )),phase),by.x=phase,by.y=phase,all.x = TRUE)
-}
+  dat = sa4 %>% 
+    arrange(desc(phase)) %>%
+    mutate(windows_3month=cut(phase, seq(min(phase), max(phase), 3))) %>%
+    mutate(windows_6month=cut(phase, seq(min(phase), max(phase), 6))) %>%
+    group_by(phase) %>% 
+    mutate(z_score_group = log(price) - mean(log(price)) / sd(log(price))) %>%  
+    ungroup %>% 
+    group_by(windows_3month) %>%
+ 
+    mutate(roll3_z_score=scale(log(price))) %>%
+    ungroup %>% 
+    group_by(windows_6month) %>%
+    mutate(roll6_z_score=scale(price)) %>%
+    arrange(desc(phase))
+  
+  
+  dat$outlier_3window<-ifelse(abs(dat$roll3_z_score)>outlier_threshold, 1, 0)
+  cleaned_3window<-subset(dat,outlier_3window == 0)
+  
+  
+  
+  cleaned_median3 = cleaned_3window %>%
+    group_by(phase) %>%
+    summarise(median=median(price),n=n())
+  
+  
+  plot(cleaned_median3$phase,cleaned_median3$median)
+  phase<-cleaned_median3$phase
+  assign(paste0('index_outlier_',i),data.frame(indexVal=priceIndex( "median","n", 1, cleaned_median3,method="Laspeyres" ),phase,median=cleaned_median3$median))
+  
+} 
 
 
 
@@ -107,18 +133,26 @@ for (i in sa4_NSW$sa4){
 
 ####### Apply a loess smoother of 10 ( own dataframe)
 
-for (i in sa4_NSW$sa4){
-  loessMod10 <- loess(indexVal ~ phase, data=eval(as.name(paste0('index_',i))), span=0.10) # 10% smoothing span
-  assign(paste0('index_smoothed_',i),predict(loessMod10)) 
+
+for (i in sa4_NSW){
   
-  #plot(eval(as.name(paste0('index_',i)))$indexVal, x=eval(as.name(paste0('index_',i)))$phase, type="l", main=paste0('index_SA4_',i), xlab="Phase", ylab="Index")
-  #lines(eval(as.name(paste0('index_smoothed_',i))), x=eval(as.name(paste0('index_',i)))$phase[1:length(eval(as.name(paste0('index_smoothed_',i))))], col="blue")
-  
-  
-  assign(paste0('df_index_smoothed_',i),data.frame(sa4=eval(i),property_type_id=1,smoothed_indexVal=eval(as.name(paste0('index_smoothed_',i))),phase=eval(as.name(paste0('index_',i)))$phase[1:length(eval(as.name(paste0('index_smoothed_',i))))]))
+  if(nrow(eval(as.name(paste0('index_outlier_',i)))) < 20 ){
+    print("skipping because low counts")
+    next
+  }
   
   
-  #dbWriteTable(con, "sa4_index_smoothed", eval(as.name(paste0('df_index_smoothed_',i))), append = TRUE)
+  loessMod10 <- loess(indexVal ~ phase, data=eval(as.name(paste0('index_outlier_',i))), span=0.10) # 10% smoothing span
+  assign(paste0('index_smoothed_outlier_',i),predict(loessMod10)) 
+  
+  
+  plot((eval(as.name(paste0('index_outlier_',i)))$indexVal), x=eval(as.name(paste0('index_outlier_',i)))$phase, type="l", main=paste0('index_SA4_',i), xlab="Phase", ylab="Index")
+  lines((eval(as.name(paste0('index_smoothed_outlier_',i)))), x=eval(as.name(paste0('index_outlier_',i)))$phase[1:length(eval(as.name(paste0('index_smoothed_outlier_',i))))], col="blue")
+  
+  
+  assign(paste0('df_index_smoothed_',i),data.frame(sa4=eval(i),property_type_id=1,smoothed_indexVal=(eval(as.name(paste0('index_smoothed_outlier_',i)))),phase=eval(as.name(paste0('index_outlier_',i)))$phase[1:length(eval(as.name(paste0('index_smoothed_outlier_',i))))]))
+  dbWriteTable(con, "sa4_index_smoothed", eval(as.name(paste0('df_index_smoothed_',i))), append = TRUE)
+  
   
 }
 
@@ -143,7 +177,7 @@ sa4_indices<-data.frame(phase)
 ## Get SA4's for NSW
 query<-dbSendQuery(con,glue::glue(" SELECT distinct sa4_16code as sa4
                                     FROM public.abs_geographies
-                                    where state = '{state}' "))
+                                     "))
 
 sa4_NSW<-dbFetch(query)
 dbClearResult(query)
@@ -151,41 +185,60 @@ dbClearResult(query)
 
 
 ###### Generate Index for each SA4 ( Creates individual dataframes for each)
-for (i in sa4_NSW$sa4){
+for (i in sa4_NSW){
   print(paste0("Doing SA4 = ",i))
-  query<-dbSendQuery(con,glue::glue("select  sa4, phase, percentile_disc(0.5) within group (order by price) as median, count(price) as n
-        from (
-            select ag.sa4_16code as sa4, se.price, pl.phase
+  
+  
+  query<-dbSendQuery(con,glue::glue("select ag.sa4_16code as sa4, se.price, pl.phase,se.address_id 
             from abs_geographies ag
             join sale_event se on se.address_id = ag.address_id
             JOIN phase_lookup pl
                 ON pl.YEAR = date_part('year', se.contract_date )
                 and pl.month = date_part('month', se.contract_date )
             JOIN address a ON a.id = ag.address_id
-            where a.property_type ='{property_type}'
+            where a.property_type ='{property_type}' and se.price > 200000 
             and ag.address_id in (
                 select ag.address_id from abs_geographies ag
                 join sale_event se on ag.address_id = se.address_id
                 where ag.sa4_16code = {i}
-                and se.price between 200000 and 6000000
-                group by ag.address_id having count(se.id) between 2 and 10
-            )
-            and se.price between 200000 and 6000000
-        ) index_data
-        group by sa4, phase
-        order by sa4 , phase
+                group by ag.address_id having count(se.id) between 2 and 10)
         "))
   
   sa4<-dbFetch(query)
   dbClearResult(query)
   
   
-  phase<-sa4$phase
-  assign(paste0('index_',i),data.frame(indexVal=priceIndex( "median","n", 1, sa4,method="Laspeyres" ),phase))
-  #sa4_indices<-merge(sa4_indices,data.frame(assign(paste0('index_',i),priceIndex( "median","n", 1, sa4,method="Laspeyres" )),phase),by.x=phase,by.y=phase,all.x = TRUE)
-}
-
-
+  dat = sa4 %>% 
+    arrange(desc(phase)) %>%
+    mutate(windows_3month=cut(phase, seq(min(phase), max(phase), 3))) %>%
+    mutate(windows_6month=cut(phase, seq(min(phase), max(phase), 6))) %>%
+    group_by(phase) %>% 
+    mutate(z_score_group = log(price) - mean(log(price)) / sd(log(price))) %>%  
+    ungroup %>% 
+    group_by(windows_3month) %>%
+    
+    mutate(roll3_z_score=scale(log(price))) %>%
+    ungroup %>% 
+    group_by(windows_6month) %>%
+    mutate(roll6_z_score=scale(price)) %>%
+    arrange(desc(phase))
+  
+  
+  dat$outlier_3window<-ifelse(abs(dat$roll3_z_score)>outlier_threshold, 1, 0)
+  cleaned_3window<-subset(dat,outlier_3window == 0)
+  
+  
+  
+  cleaned_median3 = cleaned_3window %>%
+    group_by(phase) %>%
+    summarise(median=median(price),n=n())
+  
+  
+  plot(cleaned_median3$phase,cleaned_median3$median)
+  phase<-cleaned_median3$phase
+  assign(paste0('index_outlier_',i),data.frame(indexVal=priceIndex( "median","n", 1, cleaned_median3,method="Laspeyres" ),phase,median=cleaned_median3$median))
+  
+} 
 
 
 
@@ -196,13 +249,9 @@ for (i in sa4_NSW$sa4){
   loessMod10 <- loess(indexVal ~ phase, data=eval(as.name(paste0('index_',i))), span=0.10) # 10% smoothing span
   assign(paste0('index_smoothed_',i),predict(loessMod10)) 
   
-  #plot(eval(as.name(paste0('index_',i)))$indexVal, x=eval(as.name(paste0('index_',i)))$phase, type="l", main=paste0('index_SA4_',i), xlab="Phase", ylab="Index")
-  #lines(eval(as.name(paste0('index_smoothed_',i))), x=eval(as.name(paste0('index_',i)))$phase[1:length(eval(as.name(paste0('index_smoothed_',i))))], col="blue")
-  
+
   
   assign(paste0('df_index_smoothed_',i),data.frame(sa4=eval(i),property_type_id=2,smoothed_indexVal=eval(as.name(paste0('index_smoothed_',i))),phase=eval(as.name(paste0('index_',i)))$phase[1:length(eval(as.name(paste0('index_smoothed_',i))))]))
-  
-  
-  #dbWriteTable(con, "sa4_index_smoothed", eval(as.name(paste0('df_index_smoothed_',i))), append = TRUE)
+  dbWriteTable(con, "sa4_index_smoothed", eval(as.name(paste0('df_index_smoothed_',i))), append = TRUE)
   
 }
